daae71afebfa2d246d10c073a3e5291f71831d01
nea <nea@nea.moe>
2023-06-28 16:51:48 +0200

    Fix pack200 provider in workflow

--- minecraft/build.gradle.kts
+++ minecraft/build.gradle.kts
@@ -1,9 +1,15 @@
+import dev.architectury.pack200.java.Pack200Adapter
+
 plugins {
     java
     id("gg.essential.loom") version "0.10.0.+"
     id("dev.architectury.architectury-pack200") version "0.1.3"
 }
-
+loom {
+    forge {
+        pack200Provider.set(Pack200Adapter())
+    }
+}
 dependencies {
     minecraft("com.mojang:minecraft:1.8.9")
     mappings("de.oceanlabs.mcp:mcp_stable:22-1.8.9")
